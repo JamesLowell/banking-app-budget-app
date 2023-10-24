459b185b4f942fe87ccee17f9dbca6f48d78053c
shinichikudo000 <acostayuukichi@gmail.com>
2023-10-24 15:03:48 +0800

    3rd commit

---
+++ banking-budget-app/src/BankingApp.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const BankingApp = () => {
+  return (
+    <div>BankingApp</div>
+  )
+}
+
+export default BankingApp
---
+++ banking-budget-app/src/BudgetApp.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const BudgetApp = () => {
+  return (
+    <div>BudgetApp</div>
+  )
+}
+
+export default BudgetApp
