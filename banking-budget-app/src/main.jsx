f78f58030631afa9740ac13a06c98eab798897f5
jamillessn <jamillerosesison1@gmail.com>
2023-10-31 20:20:09 +0800

    updated dashboard and MUI install

--- banking-budget-app/src/main.jsx
+++ banking-budget-app/src/main.jsx
@@ -1,12 +1,30 @@
-import React from 'react'
+
+/* eslint-disable no-unused-vars */
+import React from 'react';
 import ReactDOM from 'react-dom/client'
-import BudgetApp from './BudgetApp'
-import BankingApp from './BankingApp'
-import './index.css'
+import Login from './pages/login';
+import Register from './pages/register';
+import Dashboard from './pages/dboard-ref';
+
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {
+    path:"/",
+    element: <Login />,
+    index:true
+  },
+  {
+    path:"/register",
+    element: <Register />,
+  },
+  {
+    path:"/dashboard",
+    element: <Dashboard />,
+  },
+]
+);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <BudgetApp/>
-    <BankingApp/>
-  </React.StrictMode>,
+  <RouterProvider router = {router} />
 )
